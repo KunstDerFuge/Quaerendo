a7c079d45fc9a352257e290dfa6903a6ea14c6ca
Robert Amour <robert@ferretech.io>
2020-05-15 00:25:34 -0700

    Add basic __str__ methods to all models

--- api/models.py
+++ api/models.py
@@ -5,6 +5,9 @@ class Entity(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return 'Entity: {}'.format(self.name)
+
 
 class Source(models.Model):
     url = models.URLField(max_length=200, blank=True)
@@ -12,11 +15,17 @@ class Source(models.Model):
     associated_entities = models.ManyToManyField(Entity)
     date_retrieved = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return 'Source: {} | {}'.format(self.description[:20], self.url[:20])
+
 
 class Claim(models.Model):
     source = models.ForeignKey(Source, related_name='related_claims', on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     evidence = models.ManyToManyField(Source, through='ClaimEvidence', blank=True)
+
+    def __str__(self):
+        return 'Claim: {} | {}'.format(self.description[:20], self.source.url[:20])
 
 
 class ClaimEvidence(models.Model):
@@ -32,3 +41,6 @@ class ClaimEvidence(models.Model):
     evidence_relationship = models.CharField(choices=EvidenceRelationship.choices, max_length=25)
     description = models.TextField(blank=True)
     verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return 'Evidence {} claim {} | {}'.format(self.evidence_relationship, str(self.claim), str(self.source))
