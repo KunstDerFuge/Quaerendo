d585185f52f83ea1a9aa4d5da0ce57102fc00d7b
Robert Amour <robert@ferretech.io>
2020-05-16 03:25:24 -0700

    Add users app with basic Profile model

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=400, blank=True)
+    num_contributions = models.IntegerField()
