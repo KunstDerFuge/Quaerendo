0c0599229ff539e5325babac6dda9306054fe79e
Robert Amour <robert@ferretech.io>
2020-05-16 03:29:25 -0700

    Add is_trusted_contributor field to Profile model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,4 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=400, blank=True)
     num_contributions = models.IntegerField()
+    is_trusted_contributor = models.BooleanField(default=False)
