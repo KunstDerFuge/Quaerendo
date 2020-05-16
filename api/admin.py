ea9a4f0138cd6bf40eca96ecf9effb8c73b9dc7b
Robert Amour <robert@ferretech.io>
2020-05-15 21:32:53 -0700

    Rename ClaimEvidence model to 'Evidence'

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Entity, Source, Claim, ClaimEvidence
+from .models import Entity, Source, Claim, Evidence
 
 admin.site.register(Entity)
 admin.site.register(Source)
 admin.site.register(Claim)
-admin.site.register(ClaimEvidence)
+admin.site.register(Evidence)
