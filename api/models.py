d3a0dc1b2ae6f4cd41c4b8b0f2c33d93bad2f452
Robert Amour <robert@ferretech.io>
2020-05-18 15:52:37 -0700

    Make Topic experts field ManyToMany relation

--- api/models.py
+++ api/models.py
@@ -23,7 +23,7 @@ class Source(models.Model):
 
 class Topic(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    experts = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='expert_in')
+    experts = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='expert_in')
 
 
 class Claim(models.Model):
