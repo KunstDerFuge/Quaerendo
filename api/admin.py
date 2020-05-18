879ff0191396496b510783aa0463c601cbf75716
Robert Amour <robert@ferretech.io>
2020-05-18 14:51:25 -0700

    Add EvidenceReviews to Admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Entity, Source, Claim, Evidence
+from .models import Entity, Source, Claim, Evidence, EvidenceReview
 
 admin.site.register(Entity)
 admin.site.register(Source)
 admin.site.register(Claim)
 admin.site.register(Evidence)
+admin.site.register(EvidenceReview)
