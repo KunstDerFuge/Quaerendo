548697fc8f6509dc2757faeb1b0a2e902aeb6a66
Robert Amour <robert@ferretech.io>
2020-05-18 16:06:10 -0700

    Add Topic to Admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Entity, Source, Claim, Evidence, EvidenceReview
+from .models import Entity, Source, Claim, Evidence, EvidenceReview, Topic
 
 admin.site.register(Entity)
 admin.site.register(Source)
 admin.site.register(Claim)
 admin.site.register(Evidence)
 admin.site.register(EvidenceReview)
+admin.site.register(Topic)
