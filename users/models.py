8e5118ffd78ac4af045d25e0d026b43c2992025e
Robert Amour <robert@ferretech.io>
2020-05-18 01:28:05 -0700

    Remove num_contributions field

--- users/models.py
+++ users/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     bio = models.TextField(max_length=400, blank=True)
-    num_contributions = models.IntegerField()
     is_trusted_contributor = models.BooleanField(default=False)
