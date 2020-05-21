ce987dc2332372b42fc15e0325cc711635c70d81
Robert Amour <robert@ferretech.io>
2020-05-21 01:05:39 -0700

    Begin to put together some semblance of a frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,35 +1,69 @@
-import React, { useState } from 'react'
-import { render } from 'react-dom'
-import axios from 'axios'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CommentRoundedIcon from '@material-ui/icons/CommentRounded'
+import RateReviewRoundedIcon from '@material-ui/icons/RateReviewRounded'
+import { Divider, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { HeaderAppBar } from './HeaderAppBar'
+import ThemeProvider from '@material-ui/styles/ThemeProvider'
+import MainTheme from './MainTheme'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    height: 1000
+  },
+  fullWidth: {
+    width: '100%'
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: 240
+  },
+  content: {
+    flexGrow: 1
+  }
+}))
 
 function App () {
-  const [data, setData] = useState(null)
-
-  if (!data) {
-    axios.get('/api/claims/')
-      .then(res => {
-        console.log(res.data)
-        setData(res.data)
-      })
-  }
+  const classes = useStyles()
 
   return (
-    <div>
-      {
-        data !== null ?
-          data.map((claim, index) => {
-            return (
-              <div key={index}>{claim.claim_text}</div>
-            )
-          })
-          :
-          ''
-      }
-    </div>
+    <ThemeProvider theme={MainTheme}>
+      <div className={classes.root}>
+        <Drawer
+          className={classes.drawer}
+          variant='permanent'
+          classes={{
+            paper: classes.drawerPaper
+          }}
+          anchor="left"
+        >
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <CommentRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary='Popular Claims' />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <RateReviewRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary='Review' />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <HeaderAppBar />
+          Testing test
+        </main>
+      </div>
+    </ThemeProvider>
   )
 }
 
 export default App
-
-const container = document.getElementById('app')
-render(<App />, container)
---
+++ frontend/src/components/HeaderAppBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+
+export function HeaderAppBar() {
+  return (
+    <AppBar position='static'>
+      <Toolbar variant='dense'>
+        <Typography variant='h6'>
+          Test
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
