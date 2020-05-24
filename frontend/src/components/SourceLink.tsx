93f8b8bd2f45a6fa34a4355c089e6ab055c62c57
Robert Amour <robert@ferretech.io>
2020-05-23 20:40:31 -0700

    Update SourceLink styling

--- frontend/src/components/SourceLink.tsx
+++ frontend/src/components/SourceLink.tsx
@@ -2,31 +2,39 @@ import * as React from 'react'
 import { useGet } from 'restful-react'
 import { Link } from 'react-router-dom'
 import { Source } from '../openapi-types'
-import { Typography } from '@material-ui/core'
+import { Theme, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
 interface SourceLinkProps {
   id: number
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  source: {
+    margin: theme.spacing(1)
+  }
+}))
+
 const SourceLink: React.FC<SourceLinkProps> = (props) => {
+  const classes = useStyles()
   const {data} = useGet('/api/sources/' + props.id)
   let source: Source = undefined
   if (data) {
     source = data
   }
   return (
-    <>
+    <div className={classes.source}>
       {
         source ?
           <Link to={'/source/' + props.id}>
-            <Typography variant='h6'>
+            <Typography variant='body1'>
               Source
             </Typography>
           </Link>
           :
           ''
       }
-    </>
+    </div>
   )
 }
 
