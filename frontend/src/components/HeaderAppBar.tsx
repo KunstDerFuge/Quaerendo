3c8f97ca5a2b67b590b96ab94bba37655f63da2b
Robert Amour <robert@ferretech.io>
2020-05-24 16:18:09 -0700

    Add back button and contextual info to AppBar

--- frontend/src/components/HeaderAppBar.tsx
+++ frontend/src/components/HeaderAppBar.tsx
@@ -1,13 +1,43 @@
 import * as React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, IconButton, Theme, Toolbar, Typography } from '@material-ui/core'
+import { Route, Switch, useHistory } from 'react-router'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  contrastButton: {
+    color: theme.palette.primary.contrastText,
+    marginLeft: theme.spacing(-2)
+  }
+}))
+
+const BackButton: React.FC<{}> = () => {
+  const classes = useStyles()
+  const history = useHistory()
+  return (
+    <IconButton aria-label='back' className={classes.contrastButton} onClick={() => history.goBack()}>
+      <ArrowBackIcon />
+    </IconButton>
+  )
+}
 
 export function HeaderAppBar() {
   return (
     <AppBar position='static'>
       <Toolbar variant='dense'>
-        <Typography variant='h6'>
-          Test
-        </Typography>
+        <Switch>
+          <Route path='/claim/'>
+            <BackButton />
+            <Typography variant='h6'>
+              Claim
+            </Typography>
+          </Route>
+          <Route path='/'>
+            <Typography variant='h6'>
+              Claims
+            </Typography>
+          </Route>
+        </Switch>
       </Toolbar>
     </AppBar>
   )
