657d05c69fe4417a8954105f421aefac17f59eb3
Robert Amour <robert@ferretech.io>
2020-05-26 01:30:43 -0700

    Open evidence detail page on evidence click

--- frontend/src/components/EvidenceInfo.tsx
+++ frontend/src/components/EvidenceInfo.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { Evidence } from '../openapi-types'
-import { Card, Theme, Typography } from '@material-ui/core'
+import { Card, CardActionArea, CardContent, Theme, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import EvidenceChips from './EvidenceChips'
+import { useHistory } from 'react-router'
 
 interface EvidenceProps {
   evidence: Evidence
@@ -13,32 +14,36 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginLeft: theme.spacing(0.5)
   },
   evidenceCard: {
-    padding: theme.spacing(2),
     marginTop: theme.spacing(1)
   }
 }))
 
 const EvidenceInfo: React.FC<EvidenceProps> = (props) => {
   const classes = useStyles()
+  const history = useHistory()
   return (
     <Card variant='outlined' className={classes.evidenceCard}>
-      <Typography variant='h6' display='inline'>
-        {
-          props.evidence.source_of_evidence.title !== '' ?
-            props.evidence.source_of_evidence.title
-            :
-            'Untitled'
-        }
-      </Typography>
-      <EvidenceChips evidence={props.evidence} />
-      <Typography variant='body2' component='p'>
-        {
-          props.evidence.description !== '' ?
-            props.evidence.description
-            :
-            'No description has been provided for this piece of evidence.'
-        }
-      </Typography>
+      <CardActionArea onClick={() => history.push('/evidence/' + props.evidence.id)}>
+        <CardContent>
+          <Typography variant='h6' display='inline'>
+            {
+              props.evidence.source_of_evidence.title !== '' ?
+                props.evidence.source_of_evidence.title
+                :
+                'Untitled'
+            }
+          </Typography>
+          <EvidenceChips evidence={props.evidence} />
+          <Typography variant='body2' component='p'>
+            {
+              props.evidence.description !== '' ?
+                props.evidence.description
+                :
+                'No description has been provided for this piece of evidence.'
+            }
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
