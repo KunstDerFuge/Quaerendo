208c015bdfe7790e448b2ecbbaccffc41f006416
Robert Amour <robert@ferretech.io>
2020-05-28 03:12:06 -0700

    Add source info to evidence details page

--- frontend/src/components/EvidenceDetails.tsx
+++ frontend/src/components/EvidenceDetails.tsx
@@ -14,6 +14,7 @@ import AuthorsLinksList from './AuthorsLinksList'
 import { makeStyles } from '@material-ui/styles'
 import OpenInNewRoundedIcon from '@material-ui/icons/OpenInNewRounded'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import SourceInfo from './SourceInfo'
 
 interface EvidenceDetailProps {
   id: number
@@ -38,14 +39,16 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2)
   },
-  reviewsPanel: {
+  expansionPanel: {
     marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
+    marginBottom: theme.spacing(2)
   }
 }))
 
 const EvidenceDetails: React.FC<EvidenceDetailProps> = (props) => {
   const classes = useStyles()
+  const [showSource, setShowSource] = React.useState(true)
   const [showReviews, setShowReviews] = React.useState(false)
   const {data} = useGet({
     path: '/api/evidence/' + props.id
@@ -69,23 +72,6 @@ const EvidenceDetails: React.FC<EvidenceDetailProps> = (props) => {
                       'Untitled Evidence'
                   }
                 </Typography>
-                <Typography variant='body2' gutterBottom>
-                  <span>
-                    Authors: <AuthorsLinksList authors={evidence.source_of_evidence.authors} />
-                  </span>
-                </Typography>
-                <Typography>
-                  <div className={classes.margin}>
-                    {
-                      evidence.source_of_evidence.url !== '' ?
-                        <a href={evidence.source_of_evidence.url} target='_blank' className={classes.link}>
-                          Source <OpenInNewRoundedIcon fontSize='small' />
-                        </a>
-                        :
-                        'No source provided.'
-                    }
-                  </div>
-                </Typography>
                 <Typography variant='body1' component='p' className={classes.description}>
                   {
                     evidence.description !== '' ?
@@ -100,7 +86,24 @@ const EvidenceDetails: React.FC<EvidenceDetailProps> = (props) => {
           }
         </CardContent>
       </Card>
-      <div className={classes.reviewsPanel}>
+      <div className={classes.expansionPanel}>
+        <ExpansionPanel expanded={showSource} onChange={() => setShowSource(!showSource)}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography variant='h5'>Source</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            {
+              evidence ?
+                <SourceInfo source={evidence.source_of_evidence} />
+                :
+                ''
+            }
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+      <div className={classes.expansionPanel}>
         <ExpansionPanel expanded={showReviews} onChange={() => setShowReviews(!showReviews)}>
           <ExpansionPanelSummary
             expandIcon={<ExpandMoreIcon />}
