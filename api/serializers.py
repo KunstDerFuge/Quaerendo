3643f41213979cda72c01fc582407d20a17dd319
Robert Amour <robert@ferretech.io>
2020-06-01 15:11:24 -0700

    Add id to source serializer

--- api/serializers.py
+++ api/serializers.py
@@ -17,7 +17,7 @@ class SourceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Source
-        fields = ['title', 'url', 'summary', 'source_degree', 'authors', 'date_published', 'date_retrieved']
+        fields = ['id', 'title', 'url', 'summary', 'source_degree', 'authors', 'date_published', 'date_retrieved']
 
 
 class SourceLinkSerializer(serializers.ModelSerializer):
