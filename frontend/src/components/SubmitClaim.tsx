5ff8851204123df11ceb109df2dde517ab738601
Robert Amour <robert@ferretech.io>
2020-06-03 00:29:39 -0700

    Modularize submit source form

--- frontend/src/components/SubmitClaim.tsx
+++ frontend/src/components/SubmitClaim.tsx
@@ -1,24 +1,12 @@
 import * as React from 'react'
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Checkbox,
-  FormControlLabel,
-  Grid,
-  LinearProgress,
-  Link,
-  TextField,
-  Theme,
-  Typography
-} from '@material-ui/core'
+import { Button, Card, CardActions, CardContent, Grid, TextField, Theme, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
-import { Mutate, useGet } from 'restful-react'
-import { DatePicker } from '@material-ui/pickers'
+import { Mutate } from 'restful-react'
 import * as assert from 'assert'
 import { useHistory } from 'react-router'
 import InfoTooltip from './InfoTooltip'
+import SubmitSourceForm from './SubmitSourceForm'
+import { Source } from '../openapi-types'
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -55,45 +43,15 @@ const useStyles = makeStyles((theme: Theme) => ({
 const SubmitClaim: React.FC<{}> = () => {
   const classes = useStyles()
   const history = useHistory()
-  const [sourceUrl, setSourceUrl] = React.useState('')
-  const [sourceTitle, setSourceTitle] = React.useState('')
-  const [sourceAuthors, setSourceAuthors] = React.useState('')
-  const [sourceSummary, setSourceSummary] = React.useState('')
-  const [sourceDatePublished, setSourceDatePublished] = React.useState(new Date())
-  const [publishDateUnknown, setPublishDateUnknown] = React.useState(false)
-  const [showOtherFields, setShowOtherFields] = React.useState(false)
 
+  const [source, setSource] = React.useState(null)
   const [showClaimForm, setShowClaimForm] = React.useState(false)
   const [claimText, setClaimText] = React.useState('')
   const [claimDescription, setClaimDescription] = React.useState('')
 
-  const {data, loading} = useGet({
-    path: 'api/article/',
-    debounce: 500,
-    queryParams: {url: sourceUrl},
-    resolve: data => {
-      setSourceTitle(data.title)
-      setSourceSummary(data.summary)
-      setShowOtherFields(true)
-      data.date_published ?
-        setSourceDatePublished(new Date(data.date_published))
-        :
-        setPublishDateUnknown(true)
-    }
-  })
-
-  function isValidUrl(url: string) {
-    try {
-      new URL(url)
-    } catch (_) {
-      return false
-    }
-    return true
-  }
 
   function validateForm() {
     try {
-      assert(!sourceUrl || isValidUrl(sourceUrl))
       assert(claimText)
     } catch (_) {
       return false
@@ -104,113 +62,12 @@ const SubmitClaim: React.FC<{}> = () => {
   return (
     <Grid container direction='column' className={classes.root}>
       <Grid item>
+        <SubmitSourceForm setShowNextPage={(show: boolean) => setShowClaimForm(show)}
+                          setSource={(source: Source) => setSource(source)} visible={!showClaimForm} />
         {
           !showClaimForm ?
-
-            // Source form
-            <Card className={classes.card} elevation={8}>
-              <CardContent>
-                <Grid container direction='column' spacing={2}>
-                  <Grid item>
-                    <Typography variant='h5' gutterBottom>Source of Claim</Typography>
-                  </Grid>
-                  <Grid item container>
-                    <Grid item className={classes.maxSpace}>
-                      <TextField fullWidth label="Source URL" variant="outlined" value={sourceUrl}
-                                 onChange={e => setSourceUrl(e.target.value)} />
-                    </Grid>
-                    <Grid item className={classes.centerItems}>
-                      <InfoTooltip
-                        fieldName='Source URL'
-                        required={false}
-                        description={'If this source has a URL, like an article or Tweet, paste it here. Some source data may be auto-filled from the URL. Try to eliminate unnecessary parameters (the portion after the question mark) if applicable, making sure that the URL remains valid. Must begin with https:// or http://'} />
-                    </Grid>
-                    {
-                      showOtherFields ?
-                        ''
-                        :
-                        <Link onClick={() => setShowOtherFields(true)} color='secondary'>
-                          <Typography align='right' className={classes.skipUrl}>
-                            Skip, this source does not have a URL 🠖
-                          </Typography>
-                        </Link>
-                    }
-                  </Grid>
-                  {
-                    loading && sourceUrl ?
-                      <LinearProgress />
-                      :
-                      showOtherFields ?
-                        <>
-                          <Grid item container>
-                            <Grid item className={classes.maxSpace}>
-                              <TextField fullWidth label='Title' variant='outlined' value={sourceTitle}
-                                         onChange={e => setSourceTitle(e.target.value)} />
-                            </Grid>
-                            <Grid item className={classes.centerItems}>
-                              <InfoTooltip
-                                fieldName='Source Title'
-                                required={false}
-                                description={'The title of this source. Usually the headline, if an article. If the source is a tweet, use "Tweet from @User".'} />
-                            </Grid>
-                          </Grid>
-                          <Grid item container>
-                            <Grid item className={classes.maxSpace}>
-                              <TextField fullWidth multiline label='Summary' variant='outlined' value={sourceSummary}
-                                         rows={4} rowsMax={12} onChange={e => setSourceSummary(e.target.value)} />
-                            </Grid>
-                            <Grid item className={classes.centerItems}>
-                              <InfoTooltip
-                                fieldName='Source Summary'
-                                required={false}
-                                description={'Summary of the info in the source. This may be auto-generated in the case of an article. If so, please proofread and edit as needed. If this source is original research (i.e. mathematical deduction), include all necessary information here.'} />
-                            </Grid>
-                          </Grid>
-                          <Grid item container>
-                            <Grid item className={classes.maxSpace}>
-                              <DatePicker value={sourceDatePublished} label='Date Published' fullWidth
-                                          format='MMMM d, yyyy'
-                                          disabled={publishDateUnknown}
-                                          onChange={date => setSourceDatePublished(new Date(date.toString()))} />
-                              <div className={classes.alignItemsRight}>
-                                <FormControlLabel
-                                  control={
-                                    <Checkbox
-                                      checked={publishDateUnknown}
-                                      onChange={() => setPublishDateUnknown(!publishDateUnknown)}
-                                    />
-                                  }
-                                  label="I don't know the publication date"
-                                  labelPlacement='start'
-                                />
-                              </div>
-                            </Grid>
-                            <Grid item className={classes.centerItems}>
-                              <InfoTooltip
-                                fieldName='Source Publication Date'
-                                required={false}
-                                description={'The date the source was published. This may be auto-filled in the case of an article. Please double-check the value here if so.'} />
-                            </Grid>
-                          </Grid>
-                        </>
-                        :
-                        ''
-                  }
-                </Grid>
-              </CardContent>
-              {
-                showOtherFields ?
-                  <CardActions>
-                    <Button id='next' className={classes.leftMarginButton} onClick={() => setShowClaimForm(true)}>
-                      Next
-                    </Button>
-                  </CardActions>
-                  :
-                  ''
-              }
-            </Card>
+            ''
             :
-
             // Claim form
             <Card className={classes.card} elevation={8}>
               <CardContent>
@@ -255,12 +112,7 @@ const SubmitClaim: React.FC<{}> = () => {
                       <Button type='submit' onClick={() => mutate({
                         claim_text: claimText,
                         description: claimDescription,
-                        source_of_claim: {
-                          title: sourceTitle,
-                          url: sourceUrl,
-                          summary: sourceSummary,
-                          date_published: publishDateUnknown ? null : sourceDatePublished.toISOString()
-                        }
+                        source_of_claim: source
                       }).then((claim) => {
                         console.log(claim)
                         history.push('/claim/' + claim.id)
---
+++ frontend/src/components/SubmitSourceForm.tsx
@@ -0,0 +1,214 @@
+import * as React from 'react'
+import { PatchedSource, Source } from '../openapi-types'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  LinearProgress,
+  Link,
+  TextField,
+  Theme,
+  Typography
+} from '@material-ui/core'
+import InfoTooltip from './InfoTooltip'
+import { DatePicker } from '@material-ui/pickers'
+import { useHistory } from 'react-router'
+import { makeStyles } from '@material-ui/styles'
+import { useGet } from 'restful-react'
+
+interface SubmitSourceFormProps {
+  setSource: (source: PatchedSource) => void
+  setShowNextPage: (show: boolean) => void
+  visible: boolean
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    margin: theme.spacing(2),
+    width: '40em',
+    maxWidth: '90%'
+  },
+  skipUrl: {
+    margin: theme.spacing(1),
+    cursor: 'pointer'
+  },
+  leftMarginButton: {
+    marginLeft: 'auto'
+  },
+  maxSpace: {
+    flexGrow: 10
+  },
+  centerItems: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  alignItemsRight: {
+    display: 'flex',
+    justifyContent: 'end'
+  }
+}))
+
+const SubmitSourceForm: React.FC<SubmitSourceFormProps> = (props) => {
+  const classes = useStyles()
+  const history = useHistory()
+  const [sourceUrl, setSourceUrl] = React.useState('')
+  const [sourceTitle, setSourceTitle] = React.useState('')
+  const [sourceAuthors, setSourceAuthors] = React.useState('')
+  const [sourceSummary, setSourceSummary] = React.useState('')
+  const [sourceDatePublished, setSourceDatePublished] = React.useState(new Date())
+  const [publishDateUnknown, setPublishDateUnknown] = React.useState(true)
+  const [showOtherFields, setShowOtherFields] = React.useState(false)
+
+  const {data, loading} = useGet({
+    path: 'api/article/',
+    debounce: 500,
+    queryParams: {url: sourceUrl},
+    resolve: data => {
+      setSourceTitle(data.title)
+      setSourceSummary(data.summary)
+      setShowOtherFields(true)
+      setPublishDateUnknown(false)
+      data.date_published ?
+        setSourceDatePublished(new Date(data.date_published))
+        :
+        setPublishDateUnknown(true)
+    }
+  })
+
+  function isValidUrl(url: string) {
+    try {
+      new URL(url)
+    } catch (_) {
+      return false
+    }
+    return true
+  }
+
+  function handleClickNext() {
+    props.setShowNextPage(true)
+    props.setSource({
+      authors: [],
+      url: sourceUrl,
+      date_published: new Date(sourceDatePublished).toISOString(),
+      title: sourceTitle,
+      summary: sourceSummary
+    })
+  }
+
+  return (
+    <>
+      {
+        !props.visible ?
+          ''
+          :
+          <Card className={classes.card} elevation={8}>
+            <CardContent>
+              <Grid container direction='column' spacing={2}>
+                <Grid item>
+                  <Typography variant='h5' gutterBottom>Source of Claim</Typography>
+                </Grid>
+                <Grid item container>
+                  <Grid item className={classes.maxSpace}>
+                    <TextField fullWidth label="Source URL" variant="outlined" value={sourceUrl}
+                               onChange={e => setSourceUrl(e.target.value)} />
+                    {
+                      showOtherFields ?
+                        ''
+                        :
+                        <Link onClick={() => setShowOtherFields(true)} color='secondary'>
+                          <Typography align='right' className={classes.skipUrl}>
+                            Skip, this source does not have a URL 🠖
+                          </Typography>
+                        </Link>
+                    }
+                  </Grid>
+                  <Grid item className={classes.centerItems}>
+                    <InfoTooltip
+                      fieldName='Source URL'
+                      required={false}
+                      description={'If this source has a URL, like an article or Tweet, paste it here. Some source data may be auto-filled from the URL. Try to eliminate unnecessary parameters (the portion after the question mark) if applicable, making sure that the URL remains valid. Must begin with https:// or http://'} />
+                  </Grid>
+                </Grid>
+                {
+                  loading && sourceUrl ?
+                    <LinearProgress />
+                    :
+                    showOtherFields ?
+                      <>
+                        <Grid item container>
+                          <Grid item className={classes.maxSpace}>
+                            <TextField fullWidth label='Title' variant='outlined' value={sourceTitle}
+                                       onChange={e => setSourceTitle(e.target.value)} />
+                          </Grid>
+                          <Grid item className={classes.centerItems}>
+                            <InfoTooltip
+                              fieldName='Source Title'
+                              required={false}
+                              description={'The title of this source. Usually the headline, if an article. If the source is a tweet, use "Tweet from @User".'} />
+                          </Grid>
+                        </Grid>
+                        <Grid item container>
+                          <Grid item className={classes.maxSpace}>
+                            <TextField fullWidth multiline label='Summary' variant='outlined' value={sourceSummary}
+                                       rows={4} rowsMax={12} onChange={e => setSourceSummary(e.target.value)} />
+                          </Grid>
+                          <Grid item className={classes.centerItems}>
+                            <InfoTooltip
+                              fieldName='Source Summary'
+                              required={false}
+                              description={'Summary of the info in the source. This may be auto-generated in the case of an article. If so, please proofread and edit as needed. If this source is original research (i.e. mathematical deduction), include all necessary information here.'} />
+                          </Grid>
+                        </Grid>
+                        <Grid item container>
+                          <Grid item className={classes.maxSpace}>
+                            <DatePicker value={sourceDatePublished} label='Date Published' fullWidth
+                                        format='MMMM d, yyyy'
+                                        disabled={publishDateUnknown}
+                                        onChange={date => setSourceDatePublished(new Date(date.toString()))} />
+                            <div className={classes.alignItemsRight}>
+                              <FormControlLabel
+                                control={
+                                  <Checkbox
+                                    checked={publishDateUnknown}
+                                    onChange={() => setPublishDateUnknown(!publishDateUnknown)}
+                                  />
+                                }
+                                label="I don't know the publication date"
+                                labelPlacement='start'
+                              />
+                            </div>
+                          </Grid>
+                          <Grid item className={classes.centerItems}>
+                            <InfoTooltip
+                              fieldName='Source Publication Date'
+                              required={false}
+                              description={'The date the source was published. This may be auto-filled in the case of an article. Please double-check the value here if so.'} />
+                          </Grid>
+                        </Grid>
+                      </>
+                      :
+                      ''
+                }
+              </Grid>
+            </CardContent>
+            {
+              showOtherFields ?
+                <CardActions>
+                  <Button id='next' className={classes.leftMarginButton} onClick={handleClickNext}>
+                    Next
+                  </Button>
+                </CardActions>
+                :
+                ''
+            }
+          </Card>
+      }
+    </>
+  )
+}
+
+export default SubmitSourceForm
