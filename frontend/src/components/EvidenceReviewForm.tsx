0c07b8f69b7154e168468c5e125c38f816d4dc07
Robert Amour <robert@ferretech.io>
2020-06-05 19:53:21 -0700

    Add remaining fields to Evidence Review form

--- frontend/src/components/EvidenceReviewForm.tsx
+++ frontend/src/components/EvidenceReviewForm.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react'
 import { EvidenceRelationship, PatchedEvidenceReview, SourceDegree } from '../openapi-types'
-import { Button, Card, CardActions, CardContent, Grid, MenuItem, Select, Theme, Typography } from '@material-ui/core'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Link,
+  MenuItem,
+  Select, TextField,
+  Theme,
+  Typography
+} from '@material-ui/core'
 import InfoTooltip from './InfoTooltip'
 import { makeStyles } from '@material-ui/styles'
 
@@ -13,12 +24,13 @@ interface EvidenceReviewFormProps {
 const useStyles = makeStyles((theme: Theme) => ({
   card: {
     margin: theme.spacing(2),
-    width: '40em',
+    width: '50em',
     maxWidth: '90%'
   },
   maxSpace: {
     flexGrow: 10,
-    flexBasis: 'min-content'
+    flexBasis: 'min-content',
+    display: 'flex'
   },
   centerItems: {
     display: 'flex',
@@ -27,9 +39,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   alignItemsRight: {
     display: 'flex',
     justifyContent: 'end'
-  },
-  select: {
-    top: '0.4em'
   },
   leftMarginButton: {
     marginLeft: 'auto'
@@ -40,13 +49,13 @@ const EvidenceReviewForm: React.FC<EvidenceReviewFormProps> = (props) => {
   const classes = useStyles()
   const [evidenceRelationship, setEvidenceRelationship] = React.useState<EvidenceRelationship | 'placeholder'>('placeholder')
   const [trustworthiness, setTrustworthiness] = React.useState<'trustworthy' | 'untrustworthy' | 'placeholder'>('placeholder')
-  const [sourceDegree, setSourceDegree] = React.useState<SourceDegree>('ORIGINAL')
+  const [sourceDegree, setSourceDegree] = React.useState<SourceDegree | 'placeholder'>('placeholder')
   const [comments, setComments] = React.useState<string>('')
 
   function handleSubmit() {
     props.submitForm({
       deduced_evidence_relationship: evidenceRelationship as EvidenceRelationship,
-      deduced_source_degree: sourceDegree,
+      deduced_source_degree: sourceDegree as SourceDegree,
       is_reliable: trustworthiness === 'trustworthy',
       additional_comments: comments
     })
@@ -66,10 +75,9 @@ const EvidenceReviewForm: React.FC<EvidenceReviewFormProps> = (props) => {
                 </Grid>
                 <Grid item container>
                   <Grid item className={classes.maxSpace}>
-                    <Typography component='span'>
+                    <Typography component='span' className={classes.centerItems}>
                       This piece of evidence&nbsp;
                       <Select
-                        className={classes.select}
                         id='evidence-relationship'
                         aria-label='evidence relationship'
                         value={evidenceRelationship}
@@ -90,29 +98,81 @@ const EvidenceReviewForm: React.FC<EvidenceReviewFormProps> = (props) => {
                       required={false}
                       description={'The relationship of this evidence to the claim. Choose "proves" and "disproves" only with extreme discretion.'} />
                   </Grid>
-                  <Grid item container>
-                    <Grid item className={classes.maxSpace}>
-                      <Typography component='span'>
-                        I believe this source is&nbsp;
-                        <Select
-                          className={classes.select}
-                          id='trustworthy'
-                          aria-label='trustworthiness'
-                          value={trustworthiness}
-                          onChange={(e) => setTrustworthiness(e.target.value as 'trustworthy' | 'untrustworthy')}>
-                          <MenuItem value='placeholder' disabled><em>(Choose one...)</em></MenuItem>
-                          <MenuItem value='trustworthy'><strong>factual</strong>.</MenuItem>
-                          <MenuItem value='untrustworthy'><strong>not factual</strong>,&nbsp;
-                            <strong>dishonest</strong>,&nbsp;or an&nbsp;<strong>invalid source</strong>.</MenuItem>
-                        </Select>
-                      </Typography>
-                    </Grid>
-                    <Grid item className={classes.centerItems}>
-                      <InfoTooltip
-                        fieldName='Trustworthiness'
-                        required={true}
-                        description={'Do you believe this source is factual? Be as objective as possible.'} />
-                    </Grid>
+                </Grid>
+                <Grid item container>
+                  <Grid item className={classes.maxSpace}>
+                    <Typography component='span' className={classes.centerItems}>
+                      The source of this evidence is&nbsp;
+                      <Select
+                        id='source-degree'
+                        aria-label='source degree'
+                        value={sourceDegree}
+                        onChange={(e) => setSourceDegree(e.target.value as SourceDegree | 'placeholder')}>
+                        <MenuItem value='placeholder' disabled><em>(Choose one...)</em></MenuItem>
+                        <MenuItem value='PRIMARY'>a&nbsp;<strong>primary</strong>&nbsp;source.</MenuItem>
+                        <MenuItem value='SECONDARY'>a&nbsp;<strong>secondary</strong>&nbsp;source.</MenuItem>
+                        <MenuItem value='TERTIARY'>a&nbsp;<strong>tertiary</strong>&nbsp;source.</MenuItem>
+                        <MenuItem value='ORIGINAL'><strong>original research</strong>&nbsp;by the user who submitted
+                          it.</MenuItem>
+                      </Select>
+                    </Typography>
+                  </Grid>
+                  <Grid item className={classes.centerItems}>
+                    <InfoTooltip
+                      fieldName='Source Degree'
+                      required={false}
+                      description={
+                        <>
+                          <p>
+                            {'Is this a primary, secondary, or tertiary source? See definitions at '}
+                            <Link
+                              href={'https://en.wikipedia.org/wiki/Wikipedia:No_original_research#Primary,_secondary_and_tertiary_sources'}
+                              target='_blank'>
+                              Wikipedia
+                            </Link>.
+                          </p>
+                          <p>
+                            Note that, unlike Wikipedia, original research is allowed here when used appropriately.
+                          </p>
+                        </>
+                      } />
+                  </Grid>
+                </Grid>
+                <Grid item container>
+                  <Grid item className={classes.maxSpace}>
+                    <Typography component='span' className={classes.centerItems}>
+                      I believe this source is&nbsp;
+                      <Select
+                        id='trustworthy'
+                        aria-label='trustworthiness'
+                        value={trustworthiness}
+                        onChange={(e) => setTrustworthiness(e.target.value as 'trustworthy' | 'untrustworthy')}>
+                        <MenuItem value='placeholder' disabled><em>(Choose one...)</em></MenuItem>
+                        <MenuItem value='trustworthy'><strong>factual</strong>.</MenuItem>
+                        <MenuItem value='untrustworthy'><strong>not factual</strong>,&nbsp;
+                          <strong>dishonest</strong>,&nbsp;or an&nbsp;<strong>invalid source</strong>.</MenuItem>
+                      </Select>
+                    </Typography>
+                  </Grid>
+                  <Grid item className={classes.centerItems}>
+                    <InfoTooltip
+                      fieldName='Trustworthiness'
+                      required={true}
+                      description={'Do you believe this source is factual? Be as objective as possible.'} />
+                  </Grid>
+                </Grid>
+                <Grid item container>
+                  <Grid item className={classes.maxSpace}>
+                    <TextField
+                      id='additional-comments' fullWidth label='Additional Comments' multiline variant='outlined'
+                      rows={4} rowsMax={8} aria-label='additional comments' value={comments}
+                      onChange={(e) => setComments(e.target.value)} />
+                  </Grid>
+                  <Grid item className={classes.centerItems}>
+                    <InfoTooltip
+                      fieldName='Additional Comments'
+                      required={false}
+                      description='Any additional comments you have on this piece of evidence. These will be public and may not be anonymous.' />
                   </Grid>
                 </Grid>
               </Grid>
