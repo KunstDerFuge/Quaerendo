13d47264cbe9a756ea03e11301b0b77d0cac9801
Robert Amour <robert@ferretech.io>
2020-06-07 18:34:19 -0700

    Use IsAuthenticatedOrReadOnly as default permission

--- lensoftruth/settings.py
+++ lensoftruth/settings.py
@@ -63,6 +63,9 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
 }
 
