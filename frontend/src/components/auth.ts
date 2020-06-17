4d9376f61e599bd81d0e104af384f9eccb772c64
Robert Amour <robert@ferretech.io>
2020-06-16 20:07:55 -0700

    Don't include null function in createContext

--- frontend/src/components/auth.ts
+++ frontend/src/components/auth.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react'
 
-export const AuthContext = createContext({}, () => null)
+export const AuthContext = createContext({})
 
 export function useAuth() {
   return useContext(AuthContext)
