a41c1d3f9ed0b117d8fb4eb26554f638ac722675
Robert Amour <robert@ferretech.io>
2020-06-17 00:38:24 -0700

    Update user model required fields

--- users/models.py
+++ users/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     bio = models.TextField(max_length=400, blank=True)
     is_trusted_contributor = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
