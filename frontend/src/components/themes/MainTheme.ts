88f01549e70b4d5382a1fd897d008effb9a59975
Robert Amour <robert@ferretech.io>
2020-09-04 15:13:41 -0700

    Use lining nums in quadraat font

--- frontend/src/components/themes/MainTheme.ts
+++ frontend/src/components/themes/MainTheme.ts
@@ -13,18 +13,27 @@ const MainTheme = createMuiTheme({
     h4: {
       fontFamily: 'quadraat',
       fontSize: '2rem',
-      letterSpacing: 'unset'
+      letterSpacing: 'unset',
+      fontVariantNumeric: 'lining-nums'
     },
     h5: {
       fontFamily: 'quadraat',
-      fontSize: '1.6rem'
+      fontSize: '1.6rem',
+      fontVariantNumeric: 'lining-nums'
     },
     h6: {
       fontFamily: 'quadraat',
-      fontSize: '1.35rem'
+      fontSize: '1.35rem',
+      fontVariantNumeric: 'lining-nums'
     },
     body1: {
       fontFamily: 'quadraat',
+      fontSize: '1.1rem',
+      fontVariantNumeric: 'lining-nums',
+      textRendering: 'optimizeLegibility'
+    },
+    body2: {
+      fontFamily: 'gill-sans-roman',
       fontSize: '1.1rem',
       textRendering: 'optimizeLegibility'
     }
