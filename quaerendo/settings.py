253962f87996d48456728e788e4278bb96254059
Robert Amour <robert@ferretech.io>
2020-09-22 14:20:37 -0700

    Register custom User serializer for user registration

--- quaerendo/settings.py
+++ quaerendo/settings.py
@@ -64,6 +64,10 @@ REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'users.serializers.UserSerializer',
 }
 
+REST_AUTH_REGISTER_SERIALIZERS = {
+    'REGISTER_SERIALIZER': 'users.serializers.UserSerializer'
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
